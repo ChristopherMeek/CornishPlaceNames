951c98e76b44c1e05a611de6fe1cc54847e91f33
Chris Meek <chris.meek@sensemaking.uk.com>
2013-09-13 11:31:11 +0100

    _

---
+++ Server/Server/Domain/Place.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.Domain
+{
+    public class Place
+    {
+        public string Name { get; set; }
+        public string Parish { get; set; }
+        public string GridRef { get; set; }
+        public string CornishForm { get; set; }
+        public string Progress { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ Server/Server/PlaceModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Server.Domain;
+using Simple.Data;
+
+namespace Server
+{
+    public class PlaceModule : NancyModule
+    {
+        public PlaceModule()
+        {
+            Get["/places"] = ListPlaces;
+        }
+
+        private object ListPlaces(dynamic parameters)
+        {
+            if (!Request.Query.keyword.HasValue) return 500;
+
+            var db = Database.Open();
+            IEnumerable<Place> places = db.Places.FindAll(db.Places.Name.Like("%" + Request.Query.keyword + "%"));
+            return Response.AsJson(places);
+        }
+    }
+}
